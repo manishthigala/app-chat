c2c99f1e70723721d729de84f03ba3fa73d3fdb3
manishthigala <manishthigala16@gmail.com>
2026-05-09 15:52:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template, request, redirect, session
 import sqlite3
+import os
 
 app = Flask(__name__)
 app.secret_key = "secret123"
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "database.db")
+
 # Create Database
-conn = sqlite3.connect('database.db')
+conn = sqlite3.connect(DB_PATH)
 cursor = conn.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT,
-    password TEXT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT
 )
 """)
 
@@ -22,50 +26,49 @@ conn.close()
 # Home Page
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html")
 
 # Login
 @app.route('/login', methods=['POST'])
 def login():
 
-    username = request.form['username']
-    password = request.form['password']
+    username = request.form['username']
+    password = request.form['password']
 
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
 
-    cursor.execute(
-        "INSERT INTO users(username,password) VALUES(?,?)",
-        (username,password)
-    )
+    cursor.execute(
+        "INSERT INTO users(username,password) VALUES(?,?)",
+        (username, password)
+    )
 
-    conn.commit()
-    conn.close()
+    conn.commit()
+    conn.close()
 
-    session['user'] = username
+    session['user'] = username
 
-    return redirect('/dashboard')
+    return redirect('/dashboard')
 
 # Dashboard
 @app.route('/dashboard')
 def dashboard():
 
-    if 'user' not in session:
-        return redirect('/')
+    if 'user' not in session:
+        return redirect('/')
 
-    return render_template(
-        "dashboard.html",
-        user=session['user']
-    )
+    return render_template(
+        "dashboard.html",
+        user=session['user']
+    )
 
 # Logout
 @app.route('/logout')
 def logout():
 
-    session.pop('user', None)
+    session.pop('user', None)
 
-    return redirect('/')
+    return redirect('/')
 
 if __name__ == "__main__":
-
-    app.run(debug=True)  all files in single folder so chng ethe pth
+    app.run(debug=True)
